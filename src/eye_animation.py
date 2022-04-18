c5c27e71b16371efdba9147a2dfefc5dfa1f49b6
Gerry D'Ascoli <38154247+themorethegerryer@users.noreply.github.com>
2022-04-17 23:07:04 -0400

    Final build for testing

--- src/eye_animation.py
+++ src/eye_animation.py
@@ -2,6 +2,9 @@ import pygame
 import numpy as np
 import time
 import pickle
+import signal
+from sys import exit
+import os
 
 dis_width = 1000
 dis_height = 773
@@ -19,6 +22,8 @@ robot_gray = (121, 151, 179)
 
 time_interval = 0.1
 
+robot = None
+
 class Robot:
     def __init__(self):
         pygame.init()
@@ -27,15 +32,19 @@ class Robot:
         pygame.display.set_caption('Robot')
         
         self.robotface = pygame.image.load('robotface.png')
+        self.robotface_talk = pygame.image.load('robotface2.png')
         self.pupils = pygame.image.load('pupils.png')
         
         self.dis.fill(blue)
         self.dis.blit(self.robotface, (0,0))
         self.dis.blit(self.pupils,eyes_at_center)
 
-    def update(self, theta, r):
+    def update(self, theta, r, talk=False):
         self.dis.fill(blue)
-        self.dis.blit(self.robotface, (0,0))
+        if talk:
+            self.dis.blit(self.robotface_talk, (0,0))
+        else:
+            self.dis.blit(self.robotface, (0,0))
         self.dis.blit(self.pupils,((eyes_at_center[0] + eye_radius * r * np.cos(-theta)),(eyes_at_center[1] + eye_radius * r * np.sin(-theta))))
         pygame.display.update()
         
@@ -48,37 +57,53 @@ class Robot:
 
 def open_model(subject_name="Default Defaulterson", trial=0):
     filename = subject_name+"-"+str(trial)+".pkl"
-    with open("../models/"+filename,'rb') as fp:
-                model = pickle.load(fp)
+    if os.path.exists("../models/"+filename):
+        with open("../models/"+filename,'rb') as fp:
+                    model = pickle.load(fp)
+    else:
+        with open("../models/Default Defaulterson-0.pkl",'rb') as fp:
+                    model = pickle.load(fp)
     return model
+
+def handler(signum, frame):
+    robot.kill()
+    exit(1)
+    
+signal.signal(signal.SIGINT,handler)
 
 if __name__ == "__main__":
     robot = Robot()
-    # subject_name = input("Name of subject:\t")
-    # trial = input("Trial:\t")
-    # gaze_model = open_model(subject_name=subject_name, trial=trial)
-    gaze_model = open_model()
+    subject_name = input("Name of subject:\t")
+    trial = input("Trial:\t")
+    listening = input("Listening? (y/n):\t")
+    listen = True
+    if listening[0] == 'n':
+        listen = False
+    gaze_model = open_model(subject_name=subject_name, trial=trial)
+    print(subject_name,trial)
     print(gaze_model)
     
-    # blink_prob = gaze_model.model['blink_freq']/(60/time_interval)
     gaze_averted = True
+    talk = False
     timer = 0.0
-    # blinking = False
+    talk_timer = 0.0
     while True:
-        # if not blinking:
-        #     do_blink = (blink_prob < np.random.uniform())
-        #     if do_blink:
-        #         robot.blink()
         timer += time_interval
+        talk_timer += time_interval
+        
+        if talk_timer > 0.2 and not listen:
+            talk = not talk
+            talk_timer = 0.0
+    
         if not gaze_averted:
             # print("Eye contact")
-            robot.update(0,0)
+            robot.update(0,0,talk)
             if timer > gaze_model["eye_contact_length"]:
                 gaze_averted = True
                 timer = 0.0
         else:
             # print("Gaze Averted")
-            robot.update(gaze_model["gaze_aversion_theta"], gaze_model["gaze_aversion_r"])
+            robot.update(gaze_model["gaze_aversion_theta"], gaze_model["gaze_aversion_r"], talk)
             if timer > gaze_model["gaze_aversion_length"]:
                 gaze_averted = False
                 timer = 0.0
--- src/pupil_core_tracker.py
+++ src/pupil_core_tracker.py
@@ -19,8 +19,8 @@ gaze_model = None
 robot = None
 eye_contact_lens = []
 gaze_aversion_lens = []
+gaze_aversion_rs = []
 gaze_aversion_thetas = []
-gaze_aversions = 0
 blinks = 0
 
 blink_thresh = 1 #seconds
@@ -48,9 +48,10 @@ class GazeModel:
             "eye_contact_length" : 3.3, # seconds
             "gaze_aversion_length" : 0.7, # seconds
             "gaze_aversion_theta" : np.pi/3, # theta [-\pi, \pi]
-            "gaze_aversion_r" : 0.5, # normalize [0,1]
+            "gaze_aversion_r" : 0.6, # normalize [0,1]
             "gaze_aversion_freq" : 0.0, # times/min
-            "blink_freq" : 18.0 # times/min
+            "blink_freq" : 18.0, # times/min
+            "test_time" : 0.0 # seconds
             }
         
         # If subject model already exists, load it in. Otherwise, create fresh
@@ -65,10 +66,17 @@ class GazeModel:
 
 def handler(signum, frame):
     if gaze_model is not None:
-        robot.kill()
+        # robot.kill()
         print("Test terminated --- building model", flush=True)
+        
         total_time = time.perf_counter()-start_time
-        gaze_model.model['blink_freq'] = blinks/(total_time/60)
+        gaze_model.model["eye_contact_length"] = np.mean(eye_contact_lens)
+        gaze_model.model["gaze_aversion_length"] = np.mean(gaze_aversion_lens)
+        gaze_model.model["gaze_aversion_theta"] = np.mean(gaze_aversion_thetas)
+        gaze_model.model["gaze_aversion_r"] = np.mean(gaze_aversion_rs)
+        gaze_model.model["gaze_aversion_freq"] = len(gaze_aversion_lens)/(total_time/60)
+        gaze_model.model["blink_freq"] = blinks/(total_time/60)
+        gaze_model.model["test_time"] = total_time
         
         print("Test terminated --- saving model", flush=True)
         gaze_model.save_model()
@@ -80,7 +88,7 @@ signal.signal(signal.SIGINT,handler)
 
     
 if __name__ == "__main__":
-    robot = eye_animation.Robot()
+    # robot = eye_animation.Robot()
         
     ctx = zmq.Context()
     # The REQ talks to Pupil remote and receives the session unique IPC SUB PORT
@@ -106,11 +114,12 @@ if __name__ == "__main__":
     subscriber.subscribe('fixations')
     subscriber.subscribe('blinks')
     
-    # subject_name = input("Name of subject:\t")
-    # trial = input("Trial:\t")
-    # gaze_model = GazeModel(subject_name=subject_name, trial=trial)
-    gaze_model = GazeModel()
+    subject_name = input("Name of subject:\t")
+    trial = input("Trial:\t")
+    gaze_model = GazeModel(subject_name=subject_name, trial=trial)
+    # gaze_model = GazeModel()
     
+    eye_contact = False
     start_time = time.perf_counter()
     while True:
         topic, payload = subscriber.recv_multipart()
@@ -118,22 +127,38 @@ if __name__ == "__main__":
         # print(f"{topic}: {message}")
         # print(str(topic))
         if str(topic) == "b\'fixations\'":
-            print(message['norm_pos'])
+            # print(message['norm_pos'])
             x = (message['norm_pos'][0]*2)-1
             y = (message['norm_pos'][1]*2)-1
             theta = np.arctan2(y,x)
-            r = np.sqrt(np.square(x)+np.square(y))
+            r = min(1.0,np.sqrt(np.square(x)+np.square(y)))
             print("Fixation:",theta,r)
+            if r < 0.35:
+                if not eye_contact:
+                    eye_contact = True
+                    eye_contact_time = time.perf_counter()
+                    if time.perf_counter() - averted_gaze_time > 0.1 and averted_gaze_time > 0.1:
+                        gaze_aversion_lens.append(time.perf_counter() - averted_gaze_time)
+                print("Eye Contact")
+            else:
+                if eye_contact:
+                    eye_contact = False
+                    averted_gaze_time = time.perf_counter()
+                    if time.perf_counter() - eye_contact_time > 0.1 and eye_contact_time > 0.1:
+                        eye_contact_lens.append(time.perf_counter() - eye_contact_time)
+                print("Averted Gaze")
+                gaze_aversion_rs.append(r)
+                gaze_aversion_thetas.append(theta)
             # robot.update(theta,r)
-        elif str(topic) == "b\'gaze.3d.1.\'":
-            print(message['norm_pos'])
-            if np.linalg.norm(message['norm_pos']) < 10:
-                x = (message['norm_pos'][0]*2)-1
-                y = (message['norm_pos'][1]*2)-1
-                theta = np.arctan2(y,x)
-                r = np.sqrt(np.square(x)+np.square(y))
-                print("Gaze:", theta,r)
-                robot.update(theta,r)
+        # elif str(topic) == "b\'gaze.3d.1.\'":
+        #     print(message['norm_pos'])
+        #     if np.linalg.norm(message['norm_pos']) < 10:
+        #         x = (message['norm_pos'][0]*2)-1
+        #         y = (message['norm_pos'][1]*2)-1
+        #         theta = np.arctan2(y,x)
+        #         r = min(1.0,np.sqrt(np.square(x)+np.square(y)))
+        #         print("Gaze:", theta,r)
+        #         robot.update(theta,r)
         elif str(topic) == "b\'blinks\'":
             if time.perf_counter() - blink_time > blink_thresh:
                 blinks += 1
