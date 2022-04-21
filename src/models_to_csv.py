ddac0f45ef2a5792f85e9ab27c80fdb3879f60b6
Gerry D'Ascoli <38154247+themorethegerryer@users.noreply.github.com>
2022-04-21 15:52:39 -0400

    Added trial as separate column

--- src/models_to_csv.py
+++ src/models_to_csv.py
@@ -6,9 +6,12 @@ path2models = "../models/"
 
 models = []
 subjects = []
+trials = []
 for filename in os.listdir(path2models):
     if filename[-4:] == ".pkl":
-        subjects.append(filename[:-4])
+        subject = filename[:-4].split('-')
+        subjects.append(subject[0])
+        trials.append(subject[1])
         file = os.path.join(path2models,filename)
         with open(file,'rb') as fp:
             model = pickle.load(fp)
@@ -16,9 +19,9 @@ for filename in os.listdir(path2models):
 
 with open("../models.csv", 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Subject-Trial","eye_contact_length", "gaze_aversion_length", "gaze_aversion_theta", "gaze_aversion_r", "gaze_aversion_freq", "blink_freq", "test_time"])
+    writer.writerow(["Subject", "Trial", "eye_contact_length", "gaze_aversion_length", "gaze_aversion_theta", "gaze_aversion_r", "gaze_aversion_freq", "blink_freq", "test_time"])
     for i in range(len(models)):
-        row = [subjects[i]]
+        row = [subjects[i], trials[i]]
         for item in models[i].values():
             row.append(str(item))
         writer.writerow(row)
